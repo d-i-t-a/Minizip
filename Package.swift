884193b1a156791d09554f64581357d2410f4c66
Aferdita Muriqi <aferdita.muriqi@gmail.com>
2020-09-28 16:27:11 -0400

    added z linked library

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
 	targets: [
 		.target(name: "Minizip",
 			path: "Vendor",
-			linkerSettings: [.linkedLibrary("xml2")]
+			linkerSettings: [.linkedLibrary("xml2"), .linkedLibrary("z")]
 		)
 	]
 )
