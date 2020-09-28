c8d9258e3f2bf768385b67c70f445491d9874072
Aferdita Muriqi <aferdita.muriqi@gmail.com>
2020-09-28 16:25:11 -0400

    added package.swift

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+	name: "Minizip",
+	products: [
+		.library(name: "Minizip", targets: ["Minizip"]),
+	],
+	targets: [
+		.target(name: "Minizip",
+			path: "Vendor",
+			linkerSettings: [.linkedLibrary("xml2")]
+		)
+	]
+)
